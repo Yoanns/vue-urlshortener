23974c5898b376867d241063a94315d290319b2c
Yoann <yoanns@gmail.com>
2020-12-27 23:18:34 -0800

    added missing vue file

---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>URL Shortener</h1>
+
+        <p>
+            <router-link :to="{ name: 'home' }">Home</router-link>
+        </p>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
